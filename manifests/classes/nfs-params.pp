84e8ce4a324a3dbd7555973fd93d55bc65ac97bf
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2011-12-16 00:58:33 +0100

    [nfs] add new module to manage NFS client, servers and exported directories
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

---
+++ manifests/classes/nfs-params.pp
@@ -0,0 +1,113 @@
+# File::      <tt>nfs-params.pp</tt>
+# Author::    Sebastien Varrette (Sebastien.Varrette@uni.lu)
+# Copyright:: Copyright (c) 2011 Sebastien Varrette
+# License::   GPL v3
+#
+# ------------------------------------------------------------------------------
+# = Class: nfs::params
+#
+# In this class are defined as variables values that are used in other
+# nfs classes.
+# This class should be included, where necessary, and eventually be enhanced
+# with support for more OS
+#
+# == Warnings
+#
+# /!\ Always respect the style guide available
+# here[http://docs.puppetlabs.com/guides/style_guide]
+#
+# The usage of a dedicated param classe is advised to better deal with
+# parametrized classes, see
+# http://docs.puppetlabs.com/guides/parameterized_classes.html
+#
+# [Remember: No empty lines between comments and class definition]
+#
+class nfs::params {
+
+    ######## DEFAULTS FOR VARIABLES USERS CAN SET ##########################
+    # (Here are set the defaults, provide your custom variables externally)
+    # (The default used is in the line with '')
+    ###########################################
+
+    # ensure the presence (or absence) of nfs
+    $ensure = $nfs_ensure ? {
+        ''      => 'present',
+        default => "${nfs_ensure}"
+    }
+
+
+    #### MODULE INTERNAL VARIABLES  #########
+    # (Modify to adapt to unsupported OSes)
+    #######################################
+    # Client/server packages
+    $client_packagename = $::operatingsystem ? {
+        /(?i-mx:ubuntu|debian)/ => 'nfs-common',
+        default => 'nfs-utils'
+    }
+
+    $server_packagename = $::operatingsystem ? {
+        /(?i-mx:ubuntu|debian)/ => 'nfs-kernel-server',
+        default => 'nfs-utils'
+    }
+    $servicename = $::operatingsystem ? {
+        /(?i-mx:ubuntu|debian)/ => 'nfs-kernel-server',
+        default                 => 'nfs'
+    }
+    # used for pattern in a service ressource
+    $processname = $::operatingsystem ? {
+        /(?i-mx:ubuntu|debian)/ => 'nfs-kernel-server',
+        default                 => 'nfs'
+    }
+    $hasstatus = $::operatingsystem ? {
+        /(?i-mx:ubuntu|debian)/        => false,
+        /(?i-mx:centos|fedora|redhat)/ => true,
+        default => true,
+    }
+    $hasrestart = $::operatingsystem ? {
+        default => true,
+    }
+
+    # NFS exports file
+    $exportsfile = $::operatingsystem ? {
+        default => '/etc/exports',
+    }
+    $exportsfile_mode = $::operatingsystem ? {
+        default => '0644',
+    }
+    $exportsfile_owner = $::operatingsystem ? {
+        default => 'root',
+    }
+    $exportsfile_group = $::operatingsystem ? {
+        default => 'root',
+    }
+
+    $initconfigfile = $operatingsystem ? {
+        /(?i-mx:ubuntu|debian)/ => "/etc/default/nfs",
+        default => "/etc/sysconfig/nfs",
+    }
+
+
+    # $configdir = $::operatingsystem ? {
+    #     default => "/etc/nfs",
+    # }
+    # $configdir_mode = $::operatingsystem ? {
+    #     default => '0755',
+    # }
+
+    # $configdir_owner = $::operatingsystem ? {
+    #     default => 'root',
+    # }
+
+    # $configdir_group = $::operatingsystem ? {
+    #     default => 'root',
+    # }
+
+    # $pkgmanager = $::operatingsystem ? {
+    #     /(?i-mx:ubuntu|debian)/          => [ '/usr/bin/apt-get' ],
+    #     /(?i-mx:centos|fedora|redhat)/ => [ '/bin/rpm', '/usr/bin/up2date', '/usr/bin/yum' ],
+    #     default => []
+    # }
+
+
+}
+
---
+++ manifests/classes/nfs-server.pp
@@ -0,0 +1,135 @@
+# File::      <tt>nfs-server.pp</tt>
+# Author::    Sebastien Varrette (Sebastien.Varrette@uni.lu)
+# Copyright:: Copyright (c) 2011 Sebastien Varrette
+# License::   GPLv3
+#
+# ------------------------------------------------------------------------------
+# = Class: nfs::server
+#
+# Configure and manage an NFS server
+#
+# == Parameters:
+#
+# $ensure:: *Default*: 'present'. Ensure the presence (or absence) of nfs::server
+#
+# == Requires:
+#
+# n/a
+#
+# == Sample Usage:
+#
+#     import nfs::server
+#
+# You can then specialize the various aspects of the configuration,
+# for instance:
+#
+#         class { 'nfs::server':
+#             ensure => 'present'
+#         }
+#
+# == Warnings
+#
+# /!\ Always respect the style guide available
+# here[http://docs.puppetlabs.com/guides/style_guide]
+#
+#
+# [Remember: No empty lines between comments and class definition]
+#
+class nfs::server(
+    $ensure = $nfs::params::ensure
+)
+inherits nfs::client
+{
+    info ("Configuring nfs::server (with ensure = ${ensure})")
+
+    if ! ($ensure in [ 'present', 'absent' ]) {
+        fail("nfs::server 'ensure' parameter must be set to either 'absent' or 'present'")
+    }
+
+    case $::operatingsystem {
+        debian, ubuntu:         { include nfs::server::debian }
+        redhat, fedora, centos: { include nfs::server::redhat }
+        default: {
+            fail("Module $module_name is not supported on $operatingsystem")
+        }
+    }
+}
+
+# ------------------------------------------------------------------------------
+# = Class: nfs::server::common
+#
+# Base class to be inherited by the other nfs::server classes
+#
+# Note: respect the Naming standard provided here[http://projects.puppetlabs.com/projects/puppet/wiki/Module_Standards]
+class nfs::server::common {
+
+    # Load the variables used in this module. Check the nfs::server-params.pp file
+    require nfs::params
+
+    # trick to handle the fact that the package for client and server may be the
+    # same
+    if (! defined( Package["${nfs::params::server_packagename}"] )) {
+        package { "${nfs::params::server_packagename}":
+            ensure  => "${nfs::server::ensure}",
+        }
+    }
+
+    # TODO: deal with ensure != present
+    service { 'nfs-server':
+        name       => "${nfs::params::servicename}",
+        hasrestart => "${nfs::params::hasrestart}",
+        pattern    => "${nfs::params::processname}",
+        hasstatus  => "${nfs::params::hasstatus}",
+        require    => Package["${nfs::params::server_packagename}"],
+    }
+    if ($nfs::server::ensure == 'present') {
+        Service['nfs-server'] {
+            enable  => true,
+            ensure  => 'running',
+        }
+        concat { "${nfs::params::exportsfile}":
+            warn    => false,
+            mode    => "${nfs::params::exportsfile_mode}",
+            owner   => "${nfs::params::exportsfile_owner}",
+            group   => "${nfs::params::exportsfile_group}",
+            require => Package["${nfs::params::server_packagename}"],
+            notify  => Service["nfs-server"],
+        }
+
+        # Header of the exports file
+        concat::fragment { "${nfs::params::exportsfile}_header":
+            target  => "${nfs::params::exportsfile}",
+            content => template('nfs/exports_header.erb'),
+            ensure  => "${nfs::server::ensure}",
+            order   => 01,
+        }
+    }
+    else
+    {
+        Service['nfs-server'] {
+            enable => false,
+            ensure => 'stopped' ,
+        }
+    }
+
+
+    include concat::setup
+
+
+}
+
+
+# ------------------------------------------------------------------------------
+# = Class: nfs::server::debian
+#
+# Specialization class for Debian systems
+class nfs::server::debian inherits nfs::server::common { }
+
+# ------------------------------------------------------------------------------
+# = Class: nfs::server::redhat
+#
+# Specialization class for Redhat systems
+class nfs::server::redhat inherits nfs::server::common { }
+
+
+
