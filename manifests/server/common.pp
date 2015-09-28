49cc1a28ee7f0b8839363af88106d10aa66981f0
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-09-28 14:20:08 +0200

    manifests: add headers

--- manifests/server/common.pp
+++ manifests/server/common.pp
@@ -1,3 +1,8 @@
+# File::      <tt>client.pp</tt>
+# Author::    Sebastien Varrette (Sebastien.Varrette@uni.lu)
+# Copyright:: Copyright (c) 2011 Sebastien Varrette
+# License::   GPLv3
+#
 # ------------------------------------------------------------------------------
 # = Class: nfs::server::common
 #
